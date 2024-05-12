09f30efdb9acfc5522c8de48646a3d8499f9af3b
ex6istence <80631091+ex6istence@users.noreply.github.com>
2024-05-12 11:42:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let currentPlayer = "X";
+let gameEnded = false;
+let board = ["", "", "", "", "", "", "", "", ""];
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+let message = document.getElementById('message')
+
+function cellClicked(cellIndex) {
+    if (!gameEnded && board[cellIndex] === "") {
+        const cell = document.getElementById(`cell${cellIndex}`);
+        cell.textContent = currentPlayer;
+        cell.setAttribute('data-value', currentPlayer);
+        board[cellIndex] = currentPlayer;
+        if (checkWinner(currentPlayer)) {
+            message.textContent = `Игрок ${currentPlayer} победил`;
+            gameEnded = true;
+        } else if (isBoardFull()) {
+            message.textContent = 'Ничья';
+            gameEnded = true;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
+function checkWinner(player) {
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (board[a] === player && board[b] === player && board[c] === player) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isBoardFull() {
+    return board.every(cell => cell !== "");
+}
